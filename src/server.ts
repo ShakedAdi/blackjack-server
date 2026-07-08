c9fc05d61bafebac6e86aed5332932624a218bc9
ShakedAdi <shakedadi2512@gmail.com>
2026-07-08 14:13:52 +0300

    Initial commit to develop - add cors to allow the client to call the server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, {type Application, type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT: number = 42069;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/status', (req: Request, res: Response) => {
     res.status(200).json({isRunning: true});
