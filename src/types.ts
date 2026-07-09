1e3a0540596158311de51e4d58e019aa644cfe79
ShakedAdi <shakedadi2512@gmail.com>
2026-07-09 11:09:56 +0300

    feat: add Hand (player's hand), DealerHand interfaces and HandStatus type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 type Suit = "spades" | "hearts" | "diamonds" | "clubs";
 type Rank = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+type HandStatus = "playing" | "stood" | "busted" | "blackjack";
 
 export interface Card {
     id: number,
@@ -7,4 +8,16 @@ export interface Card {
     suit: Suit,
     value: number,
     cardPath: string
+}
+
+export interface Hand {
+    id: number,
+    cards: Card[],
+    status: HandStatus
+}
+
+export interface DealerHand {
+    cards: Card[],
+    isHoleCardHidden: boolean,
+    status: HandStatus
 }
