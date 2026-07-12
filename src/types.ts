7896df297cbec7d5f0a7d452ffb99fecd42da1bf
ShakedAdi <shakedadi2512@gmail.com>
2026-07-12 13:36:32 +0300

    refactor: adjust types - remove id attribute from Card (seems unnecessary)

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,6 @@ type Rank = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "
 type HandStatus = "playing" | "stood" | "busted" | "blackjack";
 
 export interface Card {
-    readonly id: number,
     readonly rank: Rank,
     readonly suit: Suit,
     readonly value: number,
