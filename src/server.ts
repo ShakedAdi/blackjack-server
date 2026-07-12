d8d7408c569a3817eca57da272b22eed1f10815a
ShakedAdi <shakedadi2512@gmail.com>
2026-07-12 11:00:41 +0300

    feat: filled the new-game endpint and add a all-games debug endpoint

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,23 @@ app.get('/status', (req: Request, res: Response) => {
     res.status(200).json({isRunning: true});
 });
 
-// game logic
+// === game logic ===
 const games = new Map<string, Game>();
 
 app.post('/new-game', (req: Request, res: Response) => {
     const gameId = randomUUID();
+    const game: Game = {
+        player: [], 
+        dealer: undefined, 
+        state: "player-turn"
+    };
+    games.set(gameId, game);
+    res.status(201).json({gameId});
+});
+
+// debug endpoint
+app.get('/all-games', (req: Request, res: Response) => {
+    res.status(200).json(Object.fromEntries(games));
 });
 
 app.listen(PORT, () => {
